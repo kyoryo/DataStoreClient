84235b730557e23d744c2238c8e61e313c6256a9
kyoryo <franz.rio19@gmail.com>
2016-01-20 10:54:00 +0700

    re-commit with git ignore

--- DataStoreClient/Program.cs
+++ DataStoreClient/Program.cs
@@ -12,7 +12,6 @@ namespace DataStoreClient
     {
         static void Main()
         {
-
             RunAsync().Wait(); //calls RunAsync and block other operation until done.
         }
         static async Task RunAsync()
