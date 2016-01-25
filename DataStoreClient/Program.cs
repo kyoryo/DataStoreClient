d1de856071980fbd878a02861053ff73130d455d
kyoryo <franz.rio19@gmail.com>
2016-01-25 12:00:15 +0700

    fix consuming

--- DataStoreClient/Program.cs
+++ DataStoreClient/Program.cs
@@ -23,30 +23,48 @@ namespace DataStoreClient
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                try
+                //try
+                //{
+                // HTTP GET (Getting resource(s))
+                #region test response single
+                //HttpResponseMessage response = await client.GetAsync("api/boards/1");
+                //if (response.IsSuccessStatusCode)
+                //{
+                //    Console.WriteLine("Success, Now reading...");
+                //    Board board = await response.Content.ReadAsAsync<Board>();
+                //    Console.WriteLine("");
+                //    Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}",
+                //        board.Id, board.OwnerId, board.Title, board.Order, board.CreatedAt, board.LastUpdateTime, board.IsDeleted);
+                //}
+                //else { Console.WriteLine("Unsuccessful\n" + response); }
+                #endregion
+                Console.WriteLine("Now consuming...");
+                HttpResponseMessage response = await client.GetAsync("api/Boards?q={}");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
                 {
-                    // HTTP GET (Getting resource(s))
-                    HttpResponseMessage response = await client.GetAsync("api/boards/1");
-                    if (response.IsSuccessStatusCode)
+                    Console.WriteLine("Success, Now reading...");
+                    var board = response.Content.ReadAsAsync<dynamic>().Result;
+                    foreach (var x in board.Data)
                     {
-                        Board board = await response.Content.ReadAsAsync<Board>();
-                        Console.WriteLine("{0}\t${1}\t{2}",
-                            board.Id, board.OwnerId, board.Title, board.Order, board.CreatedAt, board.LastUpdateTime, board.IsDeleted);
+                        //Console.WriteLine("{0}\t{1}", x.Data.Id, x.Data.OwnerId);
                     }
                 }
-                catch (HttpRequestException ex)
-                {
-
-                }
+                else { Console.WriteLine("Unsuccessful"); }
                 
-                //HTTP POST
-                /////
-                /////test variable
-                /////
-
                 
+
+                #region example, dont delete it until done
+                
+                #endregion
+                #region
+                #endregion
+
+
+
 
             }
         }
+       
     }
 }
